fdd588d0e325be74a9ad0a9fa9781c8e4329b1be
Lewisjmp <lewis.munoz@cuemby.com>
2024-11-27 15:49:28 -0500

    Walrus Values config

--- main.tf
+++ main.tf
@@ -1,9 +1,29 @@
-locals {
-  context = var.context
+#
+# Victoria Metrics Resources
+#
+resource "kubernetes_namespace" "victoria_auth" {
+  metadata {
+    name = var.namespace_name
+  }
 }
 
-module "submodule" {
-  source = "./modules/submodule"
+resource "helm_release" "victoria_auth" {
+  name       = var.helm_release_name
+  namespace  = var.namespace_name
+  repository = "https://victoriametrics.github.io/helm-charts/"
+  chart      = "victoria-metrics-auth"
+  version    = var.helm_chart_version
+  values = [
+    templatefile("${path.module}/values.yaml.tpl", {
+      ingress_enabled                 = var.ingress_enabled,
+      domain_name                     = var.domain_name,
+      dash_domain_name                = var.dash_domain_name
+      issuer_name                     = var.issuer_name
+      issuer_kind                     = var.issuer_kind
+    })
+  ]
+}
 
-  message = "Hello, submodule"
+locals {
+  context = var.context
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,57 @@
 #
+# Victoria Metrics Variables
+#
+
+variable "helm_release_name" {
+  description = "The name of the Helm release."
+  type        = string
+  default     = "victoria-logs-single"
+}
+
+variable "namespace_name" {
+  description = "The namespace where the Helm release will be installed."
+  type        = string
+  default     = "victoria-system"
+}
+
+variable "helm_chart_version" {
+  description = "The version of the victoria-logs-single Helm chart."
+  type        = string
+  default     = "0.5.2"
+}
+
+# Victoria Auth Ingress
+variable "ingress_enabled" {
+  type        = bool
+  description = "Victoria Auth Ingress Enabled"
+  default     = "false"
+}
+variable "domain_name" {
+  type        = string
+  description = "Domain name for Victoria Auth, e.g. 'dev.domainname.com'"
+  default     = "dev.domainname.com"
+}
+
+variable "dash_domain_name" {
+  type        = string
+  description = "Domain name with dash, e.g. 'dev-domainname-com'"
+  default     = "dev-domainname-com"
+}
+
+variable "issuer_name" {
+  type        = string
+  description = "Origin issuer name"
+  default     = "origin-ca-issuer"
+}
+
+variable "issuer_kind" {
+  type        = string
+  description = "Origin issuer kind"
+  default     = "ClusterOriginIssuer"
+}
+
+
+#
 # Contextual Fields
 #
 
